5cfa347725dfe404616ba24a45cedaccc5df1dea
zhaozhiming <kingzzm1982@sina.com>
2015-09-21 18:05:40 +0800

    add account controller

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,23 @@
+Template.postSubmit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var post = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
+
+    console.log("post url: " + post.url);
+
+    Meteor.call('postInsert', post, function(error, result) {
+      if (error) {
+        return alert(error.reason);
+      }
+      if (result.postExists) {
+        alert('This link has already been posted');
+      }
+      Router.go('postPage', {_id: result._id});
+    });
+
+  }
+});
