5edcc59bffa8f7a35f07e924fd1a316cc36e39f8
zhaozhiming <kingzzm1982@sina.com>
2015-09-22 10:54:06 +0800

    edit post

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,4 +1,9 @@
 Template.postItem.helpers({
+
+  ownPost: function() {
+    return this.userId === Meteor.userId();
+  },
+
   domain: function() {
       var a = document.createElement('a');
       a.href = this.url;
